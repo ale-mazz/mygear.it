6349d5b9365fa0cf678d8dee630e991cfb9cb231
Alessandro Mazzon <broodyblack@hotmail.it>
2017-11-28 18:51:29 +0100

    js animation

---
+++ javascript/hamburger.js
@@ -0,0 +1,9 @@
+function showmenu(){
+    var div = document.getElementById('drop_menu');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+}
