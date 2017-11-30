7f9e152215d9174bbf7444cf0ad5c8a58a47719b
Alessandro Mazzon <broodyblack@hotmail.it>
2017-11-30 11:13:50 +0100

    update

--- javascript/hamburger.js
+++ javascript/hamburger.js
@@ -1,9 +1,13 @@
-function showmenu(){
-    var div = document.getElementById('drop_menu');
-    if (div.style.display !== 'none') {
-        div.style.display = 'none';
-    }
-    else {
-        div.style.display = 'block';
-    }
-}
+(function (){
+    var hamburger = document.getElementById("hamburger");
+    hamburger.onclick = function () {
+        if (document.getElementsByClassName('menuOpen').length == 0) {
+            $('#menuResponsive').addClass('menuOpen').removeClass('menuClose');
+            $('#menu').css('height','auto');
+        }
+        else if(document.getElementsByClassName('menuClose').length == 0){
+            $('#drop_menu').addClass('menuClose').removeClass('menuOpen');
+            $('#menu').css('height','3.857em');
+        }
+    };
+});
