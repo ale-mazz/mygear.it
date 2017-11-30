734a3490a70cdba4003a4ccada3bfb22f73ebadc
Alessandro Mazzon <broodyblack@hotmail.it>
2017-11-30 12:37:27 +0100

    header responsive and js (hamburger)

--- javascript/hamburger.js
+++ javascript/hamburger.js
@@ -1,13 +1,12 @@
-(function (){
+$(function (){
     var hamburger = document.getElementById("hamburger");
     hamburger.onclick = function () {
         if (document.getElementsByClassName('menuOpen').length == 0) {
-            $('#menuResponsive').addClass('menuOpen').removeClass('menuClose');
-            $('#menu').css('height','auto');
+            $('#drop_menu').addClass('menuOpen').removeClass('menuClose');
+            $('#drop_menu').css('height','auto');
         }
         else if(document.getElementsByClassName('menuClose').length == 0){
             $('#drop_menu').addClass('menuClose').removeClass('menuOpen');
-            $('#menu').css('height','3.857em');
         }
     };
 });
