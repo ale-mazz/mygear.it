0487255f6c503f02c80540f9108d209c9566421b
Alessandro Mazzon <broodyblack@hotmail.it>
2017-12-20 13:44:25 +0100

    php

--- php/alimentatori.php
+++ php/alimentatori.php
@@ -1,6 +1,6 @@
 <?php
 
-$products=[
+$products =[
     ["titolo"=>"alimentatore1", "descrizione"=>"descrizione1"],
     ["titolo"=>"alimentatore2", "descrizione"=>"descrizione2"],
     ["titolo"=>"alimentatore3", "descrizione"=>"descrizione3"],
@@ -12,13 +12,13 @@ $insertproduct="";
 
 foreach ($products as $product){
 
-    $get_product=file_get_contents("products.html");
-    $get_product=str_replace("$title$",$product["titolo"]);
-    $get_product=str_replace("$avaible$",$product["descrizione"]);
+    $get_product=file_get_contents("product.html");
+    $get_product=str_replace('$title$',$product["titolo"], $get_product);
+    $get_product=str_replace('$avaible$',$product["descrizione"], $get_product);
     $insertproduct=$insertproduct.$get_product;
 }
 
-$catalogpage=file_get_contents("alimentatori.html");
-$catalogpage=str_replace("$products$",$insertproduct);
+$catalogpage=file_get_contents("../catalog/cataloginside/alimentatori.html");
+$catalogpage=str_replace('$products$',$insertproduct, $catalogpage);
 
 echo $catalogpage;
