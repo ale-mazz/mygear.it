9f94ecb352f38336351c9591aa05219706345f32
Alessandro Mazzon <broodyblack@hotmail.it>
2018-01-09 12:24:14 +0100

    php test user login registrazione e css

--- php/connection.php
+++ php/connection.php
@@ -7,6 +7,11 @@ $username = "root";
 $password = "";
 $db = "returnfa_mygear";
 
+define("DB_HOST", $servername);
+define("DB_NAME", $db);
+define("DB_USER", $username);
+define("DB_PASS", $password);
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
 
--- test/index.php
+++ test/index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * A simple, clean and secure PHP Login Script / MINIMAL VERSION
+ * A simple, clean and secure PHP loginclass Script / MINIMAL VERSION
  *
  * Uses PHP SESSIONS, modern password-hashing and salting and gives the basic functions a proper login system needs.
  *
@@ -12,7 +12,7 @@
 
 // checking for minimum PHP version
 if (version_compare(PHP_VERSION, '5.3.7', '<')) {
-    exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
+    exit("Sorry, Simple PHP loginclass does not run on a PHP version smaller than 5.3.7 !");
 } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
     // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
     // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
@@ -23,11 +23,11 @@ if (version_compare(PHP_VERSION, '5.3.7', '<')) {
 require_once("config/db.php");
 
 // load the login class
-require_once("classes/Login.php");
+require_once("classes/loginclass.php");
 
 // create a login object. when this object is created, it will do all login/logout stuff automatically
 // so this single line handles the entire login process. in consequence, you can simply ...
-$login = new Login();
+$login = new loginclass();
 
 // ... ask if we are logged in here:
 if ($login->isUserLoggedIn() == true) {
