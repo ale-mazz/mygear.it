b352cd49c9ed82f8f1096a53d70e249357131b6d
Alessandro Mazzon <broodyblack@hotmail.it>
2018-01-09 23:01:09 +0100

    php

--- catalog/index.php
+++ catalog/index.php
@@ -2,7 +2,6 @@
 
 require_once '../php/connection.php';
 
-
 $page=file_get_contents("catalogo.html");
 $verticalmenu=file_get_contents("verticalmenu.html");
 $footer=file_get_contents("../footer/footer.html");
@@ -18,7 +17,105 @@ $page=str_replace('$rt$','../',$page);
 $page=str_replace('$rthome$','../',$page);
 $page=str_replace('$rtcat$','cataloginside/',$page);
 
+//Prezzo Minimo
 
+$products = array();
+$qry = mysqli_query($conn,"SELECT Codice_prodotto,Prezzo FROM alimentatori ORDER BY Prezzo ASC");
+while($row = mysqli_fetch_assoc($qry)){
+    $products[] =$row;
+}
+foreach ($products as $product){
+    $page=str_replace('$priceali$',$product["Prezzo"], $page);
+}
+$products = array();
+$qry = mysqli_query($conn,"SELECT Codice_prodotto,Prezzo FROM case_pc ORDER BY Prezzo ASC");
+while($row = mysqli_fetch_assoc($qry)){
+    $products[] =$row;
+}
+foreach ($products as $product){
+    $page=str_replace('$pricecase$',$product["Prezzo"], $page);
+}
+$products = array();
+$qry = mysqli_query($conn,"SELECT Codice_prodotto,Prezzo FROM cpu ORDER BY Prezzo ASC");
+while($row = mysqli_fetch_assoc($qry)){
+    $products[] =$row;
+}
+foreach ($products as $product){
+    $page=str_replace('$pricecpu$',$product["Prezzo"], $page);
+}
+$products = array();
+$qry = mysqli_query($conn,"SELECT Codice_prodotto,Prezzo FROM cuffie ORDER BY Prezzo ASC");
+while($row = mysqli_fetch_assoc($qry)){
+    $products[] =$row;
+}
+foreach ($products as $product){
+    $page=str_replace('$pricecuffie$',$product["Prezzo"], $page);
+}
+$products = array();
+$qry = mysqli_query($conn,"SELECT Codice_prodotto,Prezzo FROM memorie_interne ORDER BY Prezzo ASC");
+while($row = mysqli_fetch_assoc($qry)){
+    $products[] =$row;
+}
+foreach ($products as $product){
+    $page=str_replace('$pricemeint$',$product["Prezzo"], $page);
+}
+$products = array();
+$qry = mysqli_query($conn,"SELECT Codice_prodotto,Prezzo FROM memorie_ram ORDER BY Prezzo ASC");
+while($row = mysqli_fetch_assoc($qry)){
+    $products[] =$row;
+}
+foreach ($products as $product){
+    $page=str_replace('$priceram$',$product["Prezzo"], $page);
+}
+$products = array();
+$qry = mysqli_query($conn,"SELECT Codice_prodotto,Prezzo FROM monitor ORDER BY Prezzo ASC");
+while($row = mysqli_fetch_assoc($qry)){
+    $products[] =$row;
+}
+foreach ($products as $product){
+    $page=str_replace('$pricemoni$',$product["Prezzo"], $page);
+}
+$products = array();
+$qry = mysqli_query($conn,"SELECT Codice_prodotto,Prezzo FROM mouse_tastiere ORDER BY Prezzo ASC");
+while($row = mysqli_fetch_assoc($qry)){
+    $products[] =$row;
+}
+foreach ($products as $product){
+    $page=str_replace('$pricemota$',$product["Prezzo"], $page);
+}
+$products = array();
+$qry = mysqli_query($conn,"SELECT Codice_prodotto,Prezzo FROM raffreddamento ORDER BY Prezzo ASC");
+while($row = mysqli_fetch_assoc($qry)){
+    $products[] =$row;
+}
+foreach ($products as $product){
+    $page=str_replace('$priceraff$',$product["Prezzo"], $page);
+}
+$products = array();
+$qry = mysqli_query($conn,"SELECT Codice_prodotto,Prezzo FROM schede_madri ORDER BY Prezzo ASC");
+while($row = mysqli_fetch_assoc($qry)){
+    $products[] =$row;
+}
+foreach ($products as $product){
+    $page=str_replace('$pricesm$',$product["Prezzo"], $page);
+}
+
+$products = array();
+$qry = mysqli_query($conn,"SELECT Codice_prodotto,Prezzo FROM schede_audio ORDER BY Prezzo ASC");
+while($row = mysqli_fetch_assoc($qry)){
+    $products[] =$row;
+}
+foreach ($products as $product){
+    $page=str_replace('$pricesa$',$product["Prezzo"], $page);
+}
+$products = array();
+$qry = mysqli_query($conn,"SELECT Codice_prodotto,Prezzo FROM schede_video ORDER BY Prezzo ASC");
+while($row = mysqli_fetch_assoc($qry)){
+    $products[] =$row;
+}
+foreach ($products as $product){
+    $page=str_replace('$pricesv$',$product["Prezzo"], $page);
+}
 
 echo $page;
 
